ca62692c5416d91f803b59038423172118347861
tomoroltuto <to1mo2.to2mo3@gmail.com>
2022-10-31 22:10:16 +0900

    first commit

---
+++ src/test/java/com/example/businessmanagement/BusinessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
